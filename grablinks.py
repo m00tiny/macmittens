8e14350ca2d1765fedf4815f834e21e93ec4faa5
m00tiny <m00tiny@users.noreply.github.com>
2022-07-12 12:41:48 -0400

    .

---
+++ grablinks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import requests
+import bs4
+import sys
+
+print('Enter target URL: ')
+target = input()
+schema = ['http://', 'https://']
+if schema not in target:
+	target = (include("type either http:// or https://")
+
+print('[+] Connecting to ' + target)
+r = requests.get(target)
+
+data = bs4.BeautifulSoup(r.text, features="html.parser")
+links = data.findAll('a')
+for links in links:
+    print(str(links), "\r\n")
+
